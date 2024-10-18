c40d123fec50078905cb11dee5406d6ddfaa2fb5
david941352 <david941352@gmail.com>
2024-10-18 19:08:15 +0900

    ca

---
+++ src/main/java/com/example/espresso/EspressoApplication.java
@@ -0,0 +1,13 @@
+package com.example.espresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspressoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspressoApplication.class, args);
+	}
+
+}
